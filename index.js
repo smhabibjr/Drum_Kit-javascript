7e22599a09d66838bed86a084efbf433cf9f5ad8
HabibJR <smhabib.abir2030@gmail.com>
2022-07-22 16:06:47 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,99 @@
+var buttons = document.querySelectorAll(".drum");
+var audio = new Audio("../sounds/crash.mp3");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", handlerClicked);
+});
+
+function handlerClicked() {
+  var value = this.innerHTML;
+  this.classList.toggle("frontcolor");
+
+  var value = this.textContent;
+
+  switch (value) {
+    case "w":
+      var audio = new Audio("../sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("../sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "s":
+      var audio = new Audio("../sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("../sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("../sounds/crash.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("../sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("../sounds/snare.mp3");
+      audio.play();
+      break;
+    default:
+      console.log("value " + value);
+  }
+}
+
+document.addEventListener("keypress", logKey);
+
+  function logKey(e) {
+    var keyCode = e.code;
+    var keyboard_key = keyCode.slice(3);
+
+    console.log("hi there " + keyboard_key.toLowerCase());
+    
+
+    switch (keyboard_key.toLowerCase()) {
+      case "w":
+        var audio = new Audio("../sounds/tom-1.mp3");
+        audio.play();
+        document.querySelector('.w').classList.toggle("frontcolor");
+        break;
+      case "a":
+        var audio = new Audio("../sounds/tom-2.mp3");
+        audio.play();
+        document.querySelector(".a").classList.toggle("frontcolor");
+        break;
+
+      case "s":
+        var audio = new Audio("../sounds/tom-3.mp3");
+        audio.play();
+        document.querySelector(".s").classList.toggle("frontcolor");
+        break;
+      case "d":
+        var audio = new Audio("../sounds/tom-4.mp3");
+        audio.play();
+        document.querySelector(".d").classList.toggle("frontcolor");
+        break;
+      case "j":
+        var audio = new Audio("../sounds/crash.mp3");
+        audio.play();
+        document.querySelector(".j").classList.toggle("frontcolor");
+        break;
+      case "k":
+        var audio = new Audio("../sounds/kick-bass.mp3");
+        audio.play();
+        document.querySelector(".k").classList.toggle("frontcolor");
+        break;
+      case "l":
+        var audio = new Audio("../sounds/snare.mp3");
+        audio.play();
+        document.querySelector(".l").classList.toggle("frontcolor");
+        break;
+      default:
+        console.log("value " + keyboard_key.toLowerCase());
+    }
+
+  }
