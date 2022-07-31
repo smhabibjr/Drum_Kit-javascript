9cd656dca7bfc2a1e8d95f12f48356ee16e1343c
HabibJR <smhabib.abir2030@gmail.com>
2022-08-01 01:35:54 +0200

    add call back function

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ buttons.forEach((button) => {
 function handlerClicked() {
   var value = this.innerHTML;
   this.classList.toggle("frontcolor");
-
   var value = this.textContent;
+switch_function(value);
+}
 
+document.addEventListener("keypress", function (e) {
+  var key_name = e.key.toLocaleLowerCase();
+  switch_function(key_name);
+});
+
+
+
+function switch_function(value){
   switch (value) {
     case "w":
       var audio = new Audio("../sounds/tom-1.mp3");
@@ -46,53 +55,10 @@ function handlerClicked() {
   }
 }
 
-document.addEventListener("keypress", function (e) {
-  console.log(e);
-  logKey(e);
-});
 
-function logKey(e) {
-  var keyCode = e.key;
-  switch (keyCode.toLowerCase()) {
-    case "w":
-      var audio = new Audio("../sounds/tom-1.mp3");
-      audio.play();
-      document.querySelector(".w").classList.toggle("frontcolor");
-      break;
-    case "a":
-      var audio = new Audio("../sounds/tom-2.mp3");
-      audio.play();
-      document.querySelector(".a").classList.toggle("frontcolor");
-      break;
 
-    case "s":
-      var audio = new Audio("../sounds/tom-3.mp3");
-      audio.play();
-      document.querySelector(".s").classList.toggle("frontcolor");
-      break;
-    case "d":
-      var audio = new Audio("../sounds/tom-4.mp3");
-      audio.play();
-      document.querySelector(".d").classList.toggle("frontcolor");
-      break;
-    case "j":
-      var audio = new Audio("../sounds/crash.mp3");
-      audio.play();
-      document.querySelector(".j").classList.toggle("frontcolor");
-      break;
-    case "k":
-      var audio = new Audio("../sounds/kick-bass.mp3");
-      audio.play();
-      document.querySelector(".k").classList.toggle("frontcolor");
-      break;
-    case "l":
-      var audio = new Audio("../sounds/snare.mp3");
-      audio.play();
-      document.querySelector(".l").classList.toggle("frontcolor");
-      break;
-    default:
-      console.log("value " + keyboard_key.toLowerCase());
-  }
-}
+
+
+
 
 
